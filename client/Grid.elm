8ccf63e7bb2fa42089a838cf8c18e8e221a4aae5
Andrew Selbie <aselbie@gmail.com>
2016-06-20 14:32:38 -0700

    Display real tile data as grid

--- client/Grid.elm
+++ client/Grid.elm
@@ -5,60 +5,35 @@ import Html
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
-yIs : Int -> Tile -> Bool
-yIs y tile = 
-  tile.y == y
+
+hexPoint : Float -> Float -> Float -> Float -> String
+hexPoint x y radius num =
+  let
+    theta = num * pi / 3
+    xCoord = toString (x + radius * (sin theta))
+    yCoord = toString (y + radius * (cos theta))
+  in
+    xCoord ++ "," ++ yCoord
 
 
-compareX : Tile -> Tile -> Order
-compareX a b =
-  compare a.x b.x
-
-
-getRow : List Tile -> Int -> List Tile
-getRow tiles rowNum =
-  List.sortWith compareX (List.filter (yIs rowNum) tiles)
-
-
-getRows : List Tile -> List (List Tile)
-getRows tiles =
-  List.map (getRow tiles) [0..9]
-
-
-
-xPoint x radius theta =
-  toString (round (x + radius * (sin (theta * pi / 3))))
-
-yPoint y radius theta =
-  toString (round (y + radius * (cos (theta * pi / 3))))
-
-point x y radius theta =
-  (xPoint x radius theta) ++ "," ++ (yPoint y radius theta)
-
+hexPoints : Float -> Float -> Float -> String
 hexPoints x y radius =
-  List.foldr (++) "" (List.intersperse " " (List.map (point x y radius) [0..5]))
+  List.foldr (++) "" (List.intersperse " " (List.map (hexPoint x y radius) [0..5]))
 
-hex radius y x =
-  polygon [ fill "white", stroke "black", strokeWidth "1", points (hexPoints x y radius)] []
 
-offset radius =
-  (sqrt 3) * radius / 2
+hexView : Tile -> Html.Html msg
+hexView tile =
+  let
+    radius = 40
+    offset = ((sqrt 3) * radius / 2)
+    shift = (rem tile.y 2) == 1
+    x = radius + offset * (toFloat tile.x) * 2 + (if shift then offset else 0)
+    y = radius + offset * (toFloat tile.y) * (sqrt 3)
+    fillColor = if tile.filled then "black" else "white"
+  in
+    polygon [ fill fillColor, stroke "black", strokeWidth "1", points (hexPoints x y radius)] []
 
-hexStart offset shift col =
-  40 + offset * col * 2 + (if shift then offset else 0)
-
-hexStarts width radius shift =
-  List.map (hexStart (offset radius) shift) [0..(width - 1)]
-
-rowY offset row=
-  40 + offset * row * (sqrt 3)
-
-hexRow radius row =
-  List.map (hex radius (rowY (offset radius) row)) (hexStarts 10 radius ((rem (round row) 2) == 0))
-
-hexRows radius =
-  List.foldr List.append [] (List.map (hexRow radius) [0..9])
 
 grid : Model -> Html.Html Msg
 grid model =
-  svg [ width "100%", height "100%", viewBox "0 0 1200 1200" ] (hexRows 40)
+  svg [ width "800px", height "800px", viewBox "0 0 800 800" ] (List.map hexView model.tiles)
